77fbf2ee4e4c2fff83f2b8c3aa71214fe2d6327c
Fozz101 <fedi.galfat@esprit.tn>
2021-03-10 04:29:32 +0100

    Remarks:
    - i implemented most of entities and associations.
    - should association inherit from user ? i implemented it as a sole
    entity.
    - i think there is an association between account-transaction and not
    credit-transaction
    - is using enumerations really usefull ? i'm talking
    about(customer/Employee/investor), let's use inheritance ? xD
    - didin't implement that reflexive association in user (we need to talk
    about it.)
    - CrudRepository is missing for the moment.

--- fynova/src/main/java/tn/fynova/spring/entities/User.java
+++ fynova/src/main/java/tn/fynova/spring/entities/User.java
@@ -1,5 +1,42 @@
 package tn.fynova.spring.entities;
 
-public class User {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+@Entity
+public class User implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int user_id;
+	private String user_password;
+	private String user_email;
+	private int user_phone;
+	
+	private String user_firstname;
+	private String user_lastname;
+	private int user_cin;
+	
+	@Temporal(TemporalType.DATE)
+	private Date user_birthday;
+	
+	@Enumerated(EnumType.STRING)
+	private Role user_role;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="claim_user")
+	private List<Claim> user_claims;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="account_user")
+	private List<Account> user_accounts;
+	
 }
